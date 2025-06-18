0248ae80c69c147abc1ff512638e3550b3cdbe7e
Viking110 <viclouniclex@gmail.com>
2025-06-18 08:32:43 +0200

    Finalisation Orienté Objet
    Finalisation de la dissociation de toutes les fonctions

--- Victor/V0.1.8/main.py
+++ Victor/V0.1.8/main.py
@@ -3,3 +3,10 @@ from reseau.decouverte import broadcast_presence, listen_for_peers, get_known_pe
 from reseau.tcp_serveur import start_tcp_server, get_messages, get_logs, get_public_keys, set_ma_cle_publique, get_groupes
 from reseau.envoi import envoyer_message, envoyer_message_multicast, envoyer_message_dans_groupe
 from reseau.groupes import creer_groupe
+from reseau.menu import afficher_menu
+
+#...
+
+if __name__ == "__main__":
+    #...
+    afficher_menu()
