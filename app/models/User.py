753b8a4f7561167bdc2b1f936f41e4c5917bb790
Aurelien Georges <maidenneo@hotmail.com>
2025-06-20 00:04:07 +0200

    test

--- app/models/User.py
+++ app/models/User.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
-
+######
 Base = declarative_base()
 
 class User(Base):
