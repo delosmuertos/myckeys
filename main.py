0ec50b59e9b52fc912214feffba805747c057e0e
Aurelien Georges <maidenneo@hotmail.com>
2025-06-20 09:47:26 +0200

    ok

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 import sys
 sys.dont_write_bytecode = True
 
-from database.db import SessionLocal, init_db
-from database.models import User
-
-from app.UserManager import UserManager
-
+from database.db import init_db
 from PyQt5.QtWidgets import QApplication
-
 from resources.views.auth_window import AuthWindow
 
 def main():
     init_db()
-    
-    #UserManager.create_user("A", "a")
     
     app = QApplication(sys.argv)
     window = AuthWindow()
